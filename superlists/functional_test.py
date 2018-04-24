aba920669512f37f45c609e1599bb34132466db1
박문수 <aspiration@kaist.ac.kr>
2018-04-24 23:57:13 +0900

    첫 기능 테스트 주석추가 및 unit test 사용.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,32 +1,42 @@
 import os
+import unittest
 
 from selenium import webdriver
 
-browser = webdriver.Chrome(
-    os.path.join(
-        os.path.dirname(
-            os.path.abspath(__file__)),
-        'chromedriver'))
 
-# 웹사이트 확인
+class NewVisitorTestCase(unittest.TestCase):
 
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Chrome(
+            os.path.join(
+                os.path.dirname(
+                    os.path.abspath(__file__)),
+                'chromedriver')
+        )
 
-# 웹페이지 타이틀에 'To-do' 있는지 확인
-assert 'To-do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-# 작업 추가하기
+    def test_can_start_a_list_adn_retrieve_it_later(self):
+        # 웹사이트 확인
+        self.browser.get('http://localhost:8000')
 
-# "공작깃털 사기"라고 텍스트 상자에 입력
+        # 웹페이지 타이틀에 'To-do' 있는지 확인
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish test')
 
-# 엔터키를 치면 페이지가 갱신되고 작업 목록에 "1: 공작깃털 사기" 아이템이 추가된다
+        # 작업 추가하기
 
-# 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재
+        # "공작깃털 사기"라고 텍스트 상자에 입력
 
-# 다시 "공작 깃털을 이용해서 그물 만들기" 라고 입력한다
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에 "1: 공작깃털 사기" 아이템이 추가된다
 
-# 페이지는 갱신되고, 두 개 아이템 목록이 보인다.
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재
 
-# 사이트가 입력한 목록을 보여주는 URL과 그에 대한 설명을 제공한다.
+        # 다시 "공작 깃털을 이용해서 그물 만들기" 라고 입력한다
 
-# 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인
+        # 페이지는 갱신되고, 두 개 아이템 목록이 보인다.
+
+        # 사이트가 입력한 목록을 보여주는 URL과 그에 대한 설명을 제공한다.
+
+        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인
