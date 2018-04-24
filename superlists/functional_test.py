e2c5a3742f6bb5d8baff5646acfa35672cc24be2
박문수 <aspiration@kaist.ac.kr>
2018-04-25 01:33:58 +0900

    기능테스트가 작업 아이템을 추가할 수 있는지 확인.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -2,6 +2,7 @@ import os
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTestCase(unittest.TestCase):
@@ -13,6 +14,7 @@ class NewVisitorTestCase(unittest.TestCase):
                     os.path.abspath(__file__)),
                 'chromedriver')
         )
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -23,15 +25,30 @@ class NewVisitorTestCase(unittest.TestCase):
 
         # 웹페이지 타이틀에 'To-do' 있는지 확인
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 작업 추가하기
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
 
         # "공작깃털 사기"라고 텍스트 상자에 입력
+        inputbox.send_keys('공작깃털 사기')
 
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows)
+        )
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재
+        self.fail('finish test')
 
         # 다시 "공작 깃털을 이용해서 그물 만들기" 라고 입력한다
 
