128648443797b0bb44866984e014a81ee4a79ddc
박문수 <aspiration@kaist.ac.kr>
2018-04-28 05:36:34 +0900

    중복 코드 제거.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -19,6 +19,11 @@ class NewVisitorTestCase(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_adn_retrieve_it_later(self):
         # 웹사이트 확인
         self.browser.get('http://localhost:8000')
@@ -41,10 +46,7 @@ class NewVisitorTestCase(unittest.TestCase):
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 "1: 공작깃털 사기" 아이템이 추가된다
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id(
-            'id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -52,12 +54,10 @@ class NewVisitorTestCase(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 페이지 갱신, 두개의 아이템이 목록에 보임
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
-        self.assertIn('2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.check_for_row_in_list_table('2: 공작깃털을 이용해서 그물 만들기')
         self.fail('finish test')
-        
+
         # 사이트가 입력한 목록을 보여주는 URL과 그에 대한 설명을 제공한다.
 
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인
