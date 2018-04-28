44e1505cc69af854a8059677342538712f366c48
박문수 <aspiration@kaist.ac.kr>
2018-04-28 18:13:29 +0900

    POST 후에 리디렉션과 탬플릿에 모든 작업 아이템 표시.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -25,31 +25,45 @@ class HomePageTest(TestCase):
         new_item_text = '신규 작업 아이템'
         request.POST['item_text'] = new_item_text
 
+        home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, new_item_text)
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        new_item_text = '신규 작업 아이템'
+        request.POST['item_text'] = new_item_text
+
         response = home_page(request)
 
-        self.assertIn(new_item_text, response.content.decode())
-        expect_html = render_to_string(
-            'home.html',
-            {'new_item_text': new_item_text}
-        )
-        self.assertEqual(response.content.decode(), expect_html)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_only_saves_items_when_POST(self):
+        request = HttpRequest()
+        home_page(request)
+
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = '첫번째 아이템'
-        first_item.save()
+        Item.objects.create(text='첫번째 아이템')
+        Item.objects.create(text='두번째 아이템')
 
-        seconde_item = Item()
-        seconde_item.text = '첫번째 아이템'
-        seconde_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        seconde_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫번째 아이템')
-        self.assertEqual(seconde_saved_item, '두번째 아이템')
+        self.assertIsNotNone(Item.objects.get(text='첫번째 아이템'))
+        self.assertIsNotNone(Item.objects.get(text='두번째 아이템'))
